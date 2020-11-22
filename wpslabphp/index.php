5632fa348d149ee175bf6b277de87f2c3eebd4dd
anishacse2018 <65843313+anishacse2018@users.noreply.github.com>
2020-11-22 15:03:35 +0530

    Create index.php

---
+++ wpslabphp/index.php
@@ -0,0 +1,7 @@
+<form action="first.php" method="post">
+Username:<input type="text" name="username"/><br>
+<br>
+Name:<input type="text" name="names"/><br>
+<br>
+<input type="submit" name="submit"/>
+</form>
