6cf25c80ef07b14094953f52b063139df30c6696
anishacse2018 <65843313+anishacse2018@users.noreply.github.com>
2020-11-26 17:09:28 +0530

    Create index.php

---
+++ wpsassign3/usingphp/index.php
@@ -0,0 +1,14 @@
+<h1>Welcome to student Login Page!!</h1>
+<br>
+<form action="first.php" method="post">
+Username
+<input type="text" name="username">
+<br>
+<br>
+Password
+<input type="password" name="password">
+<br>
+<br>
+<input type="submit" value="submit">
+<input type="reset" value="reset">
+</form>
